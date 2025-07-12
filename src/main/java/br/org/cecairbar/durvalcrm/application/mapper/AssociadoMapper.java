d643d60a3c54b1b9628b8b98f53ada91ad1b75b2
Rafael Estevam <rafestevam@gmail.com>
2025-07-12 19:22:03 -0300

    Ajuste de dependencias no mapper

--- src/main/java/br/org/cecairbar/durvalcrm/application/mapper/AssociadoMapper.java
+++ src/main/java/br/org/cecairbar/durvalcrm/application/mapper/AssociadoMapper.java
@@ -1,15 +1,29 @@
 package br.org.cecairbar.durvalcrm.application.mapper;
 
 import br.org.cecairbar.durvalcrm.application.dto.AssociadoDTO;
-import br.org.cecairbar.durvalcrm.domain.model.Associado;
+import br.org.cecairbar.durvalcrm.infrastructure.persistence.entity.AssociadoEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import java.util.List;
 
-// Usando MapStruct para o mapeamento
-@Mapper(componentModel = "jakarta")
+@Mapper(componentModel = "cdi") // Usar injeção de dependência do Quarkus
 public interface AssociadoMapper {
-    AssociadoDTO toDTO(Associado associado);
-    @Mapping(target = "id", ignore = true)
+
+    AssociadoMapper INSTANCE = Mappers.getMapper(AssociadoMapper.class);
+
+    // Mapeamento de Entidade para DTO
+    @Mapping(source = "nomeCompleto", target = "nomeCompleto")
+    // @Mapping(source = "criadoEm", target = "dataInscricao") // Removido pois dataInscricao não existe em AssociadoDTO
+    AssociadoDTO toDTO(AssociadoEntity entity);
+
+    // Mapeamento de DTO para Entidade
+    @Mapping(source = "nomeCompleto", target = "nomeCompleto")
+    @Mapping(target = "criadoEm", ignore = true) // Ignora campos gerenciados pelo BD
     @Mapping(target = "ativo", ignore = true)
-    Associado toDomain(AssociadoDTO dto);
+    AssociadoEntity toEntity(AssociadoDTO dto);
+
+    // --- SOLUÇÃO PARA O ERRO ---
+    // Adicionar explicitamente o método de mapeamento de lista
+    List<AssociadoDTO> toDTOList(List<AssociadoEntity> entities);
 }
